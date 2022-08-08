a31bcba03bb93dd6e64b309d628055c59b272d32
Mykola Harasym <henkmoody7@gmail.com>
2022-08-08 23:46:46 +0300

    start task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainerRef = document.querySelector(".gallery");
+
+function createImageMarkup({ preview, original, description }) {
+  return `    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+}
+
+const createGallery = galleryItems.map(createImageMarkup).join("");
+
+galleryContainerRef.insertAdjacentHTML("beforeend", createGallery);
+
+galleryContainerRef.addEventListener("click", onImageClick);
+function onImageClick(e) {
+  const originalSrc = e.target.dataset.source;
+  if (e.target.nodeName !== "IMG") return;
+  e.preventDefault();
+  const instance = basicLightbox.create(`
+   <img src="${originalSrc}">
+    `);
+
+  instance.show();
+
+  console.log(e.target.dataset.source);
+}
